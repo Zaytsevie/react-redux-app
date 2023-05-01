2591f420eb5c8cdfb82251441f3947aa75160fce
Igor Zaytsev <zaytsevie@gmail.com>
2023-05-01 14:43:47 +0400

    19-redux-part2-task

---
+++ src/services/todos.service.js
@@ -0,0 +1,25 @@
+import httpService from "./http.service";
+const todosEndepoint = "todos/";
+const todosService = {
+    fetch: async () => {
+        const { data } = await httpService.get(todosEndepoint, {
+            params: {
+                _page: 1,
+                _limit: 10,
+            },
+        });
+        console.log(data);
+
+        return data;
+    },
+    create: async () => {
+        const { data } = await httpService.post(todosEndepoint, {
+            title: "new title complited",
+            completed: false,
+        });
+        console.log(data);
+
+        return data;
+    },
+};
+export default todosService;
